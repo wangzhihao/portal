97634b956b2c81887a7b03da990f974636e6d7bc
zhihao wang <hao@opentown.cn>
2015-12-06 18:08:18 +0800

    add click events

---
+++ public/javascripts/script.js
@@ -0,0 +1,13 @@
+(function() {
+    $(document).ready(function() {
+        $('.philosophy').click(function() {
+            window.location.href = 'http://plato.stanford.edu'
+        });
+        $('.cs').click(function() {
+            window.location.href = 'http://en.wikipedia.org'
+        });
+        $('.music').click(function() {
+            window.location.href = 'http://168.63.208.16:5000'
+        });
+    });
+})();
